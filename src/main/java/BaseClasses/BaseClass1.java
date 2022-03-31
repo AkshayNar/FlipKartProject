5ee44c78b3e5d915d88556082fbb49968b0c174c
Akshay narhare <akshaynarhare1541@gmail.com>
2022-03-31 09:13:25 +0530

    added new else

--- src/main/java/BaseClasses/BaseClass1.java
+++ src/main/java/BaseClasses/BaseClass1.java
@@ -17,10 +17,6 @@ public class BaseClass1 {
 			WebDriverManager.chromedriver().setup();    // Here we are using WebDriverManager to set chromedriver
 
 			//System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\Browsers\\chromedriver.exe");
-			//System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\Browsers\\chromedriver.exe");
-			//System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\Browsers\\chromedriver.exe");
-			//System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\Browsers\\chromedriver.exe");
-			//System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\Browsers\\chromedriver.exe");
 			WebDriver driver = new ChromeDriver();     //upcasting
 			driver.get("https://www.flipkart.com/");
 			driver.manage().window().maximize();
@@ -35,7 +31,14 @@ public class BaseClass1 {
 			return driver;
 		}	
 		
-		
+//		else
+//		{
+//			System.setProperty("webdriver.gecko.driver", "src\\main\\resources\\Browsers\\geckodriver.exe");
+//			WebDriver driver = new FirefoxDriver();     //upcasting
+//			driver.get("https://www.flipkart.com/");
+//			driver.manage().window().maximize();
+//			return driver;
+//		}
 		
 		
 	}
