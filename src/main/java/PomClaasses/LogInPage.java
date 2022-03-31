a101f5d4651df0c9bfb7cbd1ad4727e6b96f69d7
Akshay narhare <akshaynarhare1541@gmail.com>
2022-03-31 08:15:26 +0530

    First commit

---
+++ src/main/java/PomClaasses/LogInPage.java
@@ -0,0 +1,51 @@
+package PomClaasses;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import UtilClasses.Utility1;
+
+public class LogInPage extends Utility1{
+
+	WebDriver driver;
+	@FindBy(xpath="(//input[@type='text'])[2]")
+	private WebElement emailID;
+	
+	@FindBy(xpath="//input[@type='password']")
+	private WebElement password;
+	
+	@FindBy(xpath="(//button[@type='submit'])[2]")
+	private WebElement submit;
+	
+	public LogInPage(WebDriver driver)
+	{
+		PageFactory.initElements(driver, this);
+		
+		this.driver = driver;
+	}
+	
+	public void enterEmail() throws IOException
+	{
+		//emailID.sendKeys("akshaynarhare1541@gmail.com");
+		
+		emailID.sendKeys(getConfigData("email"));
+		
+	}
+	
+	public void enterPassword() throws IOException
+	{
+		//password.sendKeys("Akshay123");
+		
+		password.sendKeys(getConfigData("password"));
+	}
+	
+	public void clickSubmit() throws InterruptedException
+	{
+		submit.click();
+		Thread.sleep(2000);
+	}
+}
